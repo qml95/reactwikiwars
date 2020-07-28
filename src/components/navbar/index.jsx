75fea63e7d59a3050dbb72bc9a4f28a69ef9c99c
quentin <qml@wedigital.garden>
2020-07-28 17:48:00 +0200

    fix navbar, add new page weapons and characters" "

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import {
-    Link,
-  } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Text from "../../lib/text";
 import { StyledNavbar, StyledLinkList, StyledLink } from './styles'
 
 const Navbar = () => (
 	<StyledNavbar>
 		<StyledLinkList>
 			<StyledLink>
-				<Link to="/">Home</Link>
+				<Link to="/">
+					<Text tag="span" size="base">
+						Accueil
+					</Text>
+				</Link>
 			</StyledLink>
 			<StyledLink>
-				<Link to="/starships">Starships</Link>
+				<Link to="/starships">
+					<Text tag="span" size="base">Vaisseaux</Text>
+				</Link>
+			</StyledLink>
+			<StyledLink>
+				<Link to="/characters">
+					<Text tag="span" size="base">Personnages</Text>
+				</Link>
+			</StyledLink>
+			<StyledLink>
+				<Link to="/weapons">
+					<Text tag="span" size="base">Armes</Text>
+				</Link>
 			</StyledLink>
 		</StyledLinkList>
 	</StyledNavbar>
--- src/components/navbar/styles.js
+++ src/components/navbar/styles.js
@@ -1,9 +1,22 @@
 import styled from 'styled-components'
+import { spacing, color } from '../../lib/_utils/branding'
 
 const StyledNavbar = styled.nav`
+	background-color: ${color.mediumGrey};
 `
-const StyledLinkList = styled.ul``
+const StyledLinkList = styled.ul`
+	display: flex;
+	justify-content: center;
+	list-style: none;
+	margin: ${spacing.none};
+	padding-bottom: ${spacing['space-2']};
+`
 
-const StyledLink = styled.li``
+const StyledLink = styled.li`
+	padding: 0 ${spacing['space-4']};
+	a {
+		text-decoration: none;
+	}
+`
 
 export { StyledNavbar, StyledLinkList, StyledLink}
